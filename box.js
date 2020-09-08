25bd1b521fd71534702d52f02a10e29c0b20437c
reshon12 <69683590+reshon12@users.noreply.github.com>
2020-09-08 19:43:24 +0530

    Add files via upload

--- box.js
+++ box.js
@@ -1,9 +1,8 @@
 class Box {
   constructor(x, y, width, height) {
     var options = {
-        'restitution':0.8,
-        'friction':0.3,
-        'density':1.0
+        'restitution':0.8
+        
     }
     this.body = Bodies.rectangle(x, y, width, height, options);
     this.width = width;
@@ -13,9 +12,13 @@ class Box {
   }
   display(){
     var pos =this.body.position;
+     var angle =this.body.angle;
+     push();
+     translate(pos.x,pos.y);
+     rotate(angle)
     rectMode(CENTER);
     fill(255);
-    rect(pos.x, pos.y, this.width, this.height);
+    rect(0,0, this.width, this.height);
     pop();
   }
 };
--- sketch.js
+++ sketch.js
@@ -3,7 +3,8 @@ const World= Matter.World;
 const Bodies = Matter.Bodies;
 
 var engine, world;
-var box1;
+var box1,box2;
+var ground;
 
 function setup(){
     var canvas = createCanvas(400,400);
@@ -11,11 +12,15 @@ function setup(){
     world = engine.world;
 
     box1 = new Box(200,300,50,50);
+    box2 = new Box(240,100,50,100);
+    ground = new  Ground(200,390,400,20);
 }
 
 function draw(){
     background(0);
     Engine.update(engine);
     box1.display();
+    box2.display();
+    ground.display();
    
 }
